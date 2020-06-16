7edf9dc5e460654d155560937034de2bdf89a962
konyan <devkonyan@gmail.com>
2020-06-16 19:22:40 +0630

    add delete button

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -37,6 +37,12 @@ class ContactsController < ApplicationController
     end
   end
 
+  def destroy
+    Contact.find(params[:id]).destroy
+    flash[:succsss] = "Contact was successfully destroy."
+    redirect_to contacts_path
+  end
+
   private
 
   def contact_params
